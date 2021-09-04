b2e38246f2af0084dd6896221ba9c1ffc3ee2b37
Tomachi <lego@aol.jp>
2021-09-05 08:01:00 +0900

    Update main.php

--- generateMarkdown/main.php
+++ generateMarkdown/main.php
@@ -169,7 +169,7 @@ foreach ($events as $event) {
 }
 file_put_contents("source/includes/_events.md", implode("\n", $markdown));
 
-$ver_sha = end(explode($version));
+$ver_sha = end(explode("_", $version));
 $github_ver_url = "https://github.com/jaoafa/MyMaid4/commit/" . $ver_sha;
 
 file_put_contents("source/includes/_version.md", "- Version: [`$version`]($github_ver_url)");
